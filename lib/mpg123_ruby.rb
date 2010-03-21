d2dc7105b553cc38f9f99d697ea9b658e13f1115
Jude Sutton <jude.sutton@gmail.com>
2010-03-21 20:00:12 +1100

    a bit of a cleanup on the handling of id3 info. still not perfect.

--- lib/mpg123_ruby.rb
+++ lib/mpg123_ruby.rb
@@ -15,37 +15,23 @@ module MPG123
       
       @mpg123_handle = MPG123::API.mpg123_new(nil, nil)
       MPG123::API.mpg123_open @mpg123_handle, @filename
-    end
-    
-    def read_id3
       MPG123::API.mpg123_scan @mpg123_handle
-      if MPG123::API.mpg123_meta_check(@mpg123_handle) == 3 #should use the const
+
+      if MPG123::API.mpg123_meta_check(@mpg123_handle) & 0x3
         @v2 = MPG123::API.get_id3_v2 @mpg123_handle
+        @v1 = MPG123::API.get_id3_v1 @mpg123_handle
       end
     end
     
-    def title
-      @v2.title.p
-    end
     
-    def artist
-      @v2.artist.p
-    end
-    
-    def album
-      @v2.album.p
-    end
-    
-    def year
-      @v2.year.p
-    end
-    
-    def genre
-      @v2.genre.p
-    end
-    
-    def comment
-      @v2.comment.p
+    def method_missing(sym, *args, &block)
+      if %w{title artist album year genre comment}.include? sym.to_s
+        if !@v2.send(sym).nil?
+          return @v2.send(sym).p
+        elsif !@v1.send(sym).nil?
+          return @v1.send(sym).strip
+        end
+      end
     end
   end
 end
